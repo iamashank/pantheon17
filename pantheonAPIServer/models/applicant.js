677487116d67c67a9ef8b0f38387a95645391275
Paritosh Sharma <paritosh0807@gmail.com>
2017-09-26 00:29:11 +0530

    Fixing bugs

--- pantheonAPIServer/models/applicant.js
+++ pantheonAPIServer/models/applicant.js
@@ -75,7 +75,7 @@ module.exports.verifyApplicant = function(name, email, phoneNumber, callback) {
 };
 
 module.exports.updateApplicant = function(applicant, callback) {
-  Applicant.findOneAndUpdate({ name: applicant.name, email: applicant.email, phoneNumber: applicant.phoneNumber }, { $set: { city: applicant.city, state: applicant.state, rollNumber: applicant.rollNumber, gender: applicant.gender, year: applicant.year, collegeName: applicant.collegeName }}).exec(callback);
+  Applicant.findOneAndUpdate({ name: applicant.name, email: applicant.email, phoneNumber: applicant.phoneNumber }, { $set: { city: applicant.city, state: applicant.state, rollNumber: applicant.rollNumber, gender: applicant.gender, year: applicant.year, collegeName: applicant.collegeName, verified: true }}).exec(callback);
 };
 
 module.exports.getAllApplicant = function(callback) {
