73822f059a13f856bdb0417096ca680abaf93314
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2017-09-28 14:20:50 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	setTimeout(function() {
     		$("#introRegister").show();
   	}, 1000);
-	$("#successMessage").html("You have successfully completed your individual registration. <br><br> Your Pantheon ID: <b>PAPAAPA</b><br>Your Email: <b>mya.sushant@yahoo.com</b><br><br>Please take a screenshot of this page. Details of the same has also been sent to your email. <br><br>What's next? Register for various events now.<br><br><div class=\"btn btn-success\" style=\"font-size:20px;padding:3%;margin-left:-10px;\" onclick=\"window.location.href='https://www.pantheon17.in/'\">View Events</div><br>");
+	$("#successMessage").html("You have successfully completed your individual registration. <br><br> Your Pantheon ID: <b>PAPAAPA</b><br>Your Email: <b>mya.sushant@yahoo.com</b><br><br>Please take a screenshot of this page. Details of the same has also been sent to your email. <br><br>What's next? Register for various events now.<br><br><div class=\"btn btn-success\" style=\"font-size:20px;padding:3%;margin-left:-10px;\" onclick=\"window.location.href='https://www.pantheon17.in/events'\">View Events</div><br>");
 });
 
 
@@ -28,21 +28,21 @@ function validate1(){
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if(name==""){
 		error = 1;
-		$("#name").css({"border":"1px solid red"});
+		$("#name").css({"border":"2px solid red"});
 	}
 	else{
 		$("#name").css({"border":""});
 	}
 	if(email=="" || re.test(email)==false){
 		error = 1;
-		$("#email").css({"border":"1px solid red"});
+		$("#email").css({"border":"2px solid red"});
 	}
 	else{
 		$("#email").css("border","");
 	}
 	if(phoneNumber=="" || isNaN(phoneNumber) || phoneNumber.length!=10){
 		error = 1;
-		$("#phoneNumber").css({"border":"1px solid red"});
+		$("#phoneNumber").css({"border":"2px solid red"});
 	}
 	else{
 		$("#phoneNumber").css("border","");
@@ -112,8 +112,8 @@ function sendOTP(){
 			grecaptcha.reset();
 	    	cancelOTP();
 	    	if(data.msg=="applicant already registered"){
-	    		$("#email").css({"border":"1px solid red"});
-				$("#phoneNumber").css({"border":"1px solid red"});
+	    		$("#email").css({"border":"2px solid red"});
+				$("#phoneNumber").css({"border":"2px solid red"});
 				$(".captcha-error").text("The Email and Phone number is already registered. Please check your Email for your Pantheon ID");
 	    	}
 	    	else if(data.msg=="Invalid Captcha"){
@@ -184,35 +184,35 @@ function validateFinal(){
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if(gender!="Male" && gender!="Female" && gender!="Others"){
 		error = 1;
-		$("#gender").css({"border":"1px solid red"});
+		$("#gender").css({"border":"2px solid red"});
 	}
 	else{
 		$("#gender").css({"border":""});
 	}
-	if(year!="1" && year!="2" && year!="3" && year!="4" && year!="5"){
+	if(year!="0" && year!="1" && year!="2" && year!="3" && year!="4" && year!="5"){
 		error = 1;
-		$("#year").css({"border":"1px solid red"});
+		$("#year").css({"border":"2px solid red"});
 	}
 	else{
 		$("#year").css({"border":""});
 	}
 	if(rollNumber==""){
 		error = 1;
-		$("#rollNumber").css({"border":"1px solid red"});
+		$("#rollNumber").css({"border":"2px solid red"});
 	}
 	else{
 		$("#rollNumber").css({"border":""});
 	}
 	if(city==""){
 		error = 1;
-		$("#city").css({"border":"1px solid red"});
+		$("#city").css({"border":"2px solid red"});
 	}
 	else{
 		$("#city").css({"border":""});
 	}
 	if(state==""){
 		error = 1;
-		$("#state").css({"border":"1px solid red"});
+		$("#state").css({"border":"2px solid red"});
 	}
 	else{
 		$("#state").css({"border":""});
@@ -221,7 +221,7 @@ function validateFinal(){
 		collegeName = $("#otherCollegeField").val();
 		if(collegeName==""){
 			error = 1;
-			$("#otherCollegeField").css({"border":"1px solid red"});
+			$("#otherCollegeField").css({"border":"2px solid red"});
 		}
 		else{
 			$("#otherCollegeField").css({"border":""});
@@ -230,7 +230,7 @@ function validateFinal(){
 	else{
 		if(collegeName==""){
 			error = 1;
-			$("#collegeName").css({"border":"1px solid red"});
+			$("#collegeName").css({"border":"2px solid red"});
 		}
 		else{
 			$("#collegeName").css({"border":""});
@@ -252,7 +252,7 @@ function validateFinal(){
 		$("#registerButton").attr("disabled","disabled");
 		$.post("https://www.pantheon17.in/api/applicants/register",finalRegister).done(function(data){
 		    if(data.success){
-		    	$("#successMessage").html("You have successfully completed your individual registration. <br><br> Your Pantheon ID: <b>"+data.id+"</b><br>Your Email: <b>"+email+"</b><br><br>Please take a screenshot of this page. Details of the same has also been sent to your email. <br><br>What's next? Register for various events now.<br><br><div class=\"btn btn-success\" style=\"font-size:20px;padding:3%;margin-left:-10px;\" onclick=\"window.location.href='https://www.pantheon17.in/'\">View Events</div><br>");
+		    	$("#successMessage").html("You have successfully completed your individual registration. <br><br> Your Pantheon ID: <b>"+data.id+"</b><br>Your Email: <b>"+email+"</b><br><br>Please take a screenshot of this page. Details of the same has also been sent to your email. <br><br>What's next? Register for various events now.<br><br><div class=\"btn btn-success\" style=\"font-size:20px;padding:3%;margin-left:-10px;\" onclick=\"window.location.href='https://www.pantheon17.in/events'\">View Events</div><br>");
 				$("#registerForm2").addClass('animated fadeOutUp');
 				setTimeout(function() {
 					$("#registerForm2").hide();
