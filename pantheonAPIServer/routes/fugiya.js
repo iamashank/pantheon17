c9c0357ce2874bdc8bbb43ab0b8cf5ff66cca7df
Paritosh Sharma <paritosh0807@gmail.com>
2017-10-03 15:42:02 +0530

    Adding fujiya api

--- pantheonAPIServer/routes/fugiya.js
+++ pantheonAPIServer/routes/fugiya.js
@@ -14,7 +14,7 @@ var storage = multer.diskStorage({
     cb(null, 'public/images/fugiya123');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname );
+    cb(null, Date.now() + '-' + file.ext );
   }
 });
 
