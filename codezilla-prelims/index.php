e4330838f0444c6c6a298f05e031acafbbcd2cdc
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2017-10-03 19:13:49 +0530

    Update index.php

--- codezilla-prelims/index.php
+++ codezilla-prelims/index.php
@@ -11,8 +11,8 @@
     <meta name="robots" content="all,follow">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Pacifico">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700">
     <link rel="stylesheet" href="style.css" id="theme-stylesheet">
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
